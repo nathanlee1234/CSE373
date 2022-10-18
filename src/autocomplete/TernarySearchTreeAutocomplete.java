ad0796d7459190b516bd346a3e070b1fb57589d4
nathanlee1234 <nathanjerlee@gmail.com>
2022-10-18 16:02:05 -0700

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/autocomplete/BinarySearchAutocomplete.java
    #	src/autocomplete/SequentialSearchAutocomplete.java

--- src/autocomplete/TernarySearchTreeAutocomplete.java
+++ src/autocomplete/TernarySearchTreeAutocomplete.java
@@ -1,9 +1,10 @@
 package autocomplete;
 
-import org.objectweb.asm.tree.analysis.Value;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
 
 /**
  * Ternary search tree (TST) implementation of the {@link Autocomplete} interface.
@@ -37,8 +38,43 @@ public class TernarySearchTreeAutocomplete implements Autocomplete {
         }
         if (c < x.data) x.left = put(x.left,  key, d);
         else if (c > x.data) x.right = put(x.right, key, d);
-        else if (d < key.length() - 1) x.mid = put(x.mid,   key, d+1);
+        else if (d < key.length() - 1) x.mid = put(x.mid, key,d+1);
+        else x.isTerm = true;
         return x;
+    }
+
+    public List<CharSequence> keysWithPrefix(String prefix) {
+        if (prefix == null) {
+            throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+        }
+        List<CharSequence> matches = new ArrayList<>();
+        Node x = get(overallRoot, prefix, 0);
+        if (x == null) return matches;
+        if (x.isTerm) matches.add(prefix);
+        collect(x.mid, new StringBuilder(prefix), matches);
+        return matches;
+    }
+
+
+
+    // return subtrie corresponding to given key
+    private Node get(Node x, String key, int d) {
+        if (x == null) return null;
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        char c = key.charAt(d);
+        if (c < x.data) return get(x.left,  key, d);
+        else if (c > x.data) return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid, key,d+1);
+        else return x;
+    }
+
+    private void collect(Node x, StringBuilder prefix, List<CharSequence> matches) {
+        if (x == null) return;
+        collect(x.left, prefix, matches);
+        if (x.isTerm) matches.add(prefix.append(x.data).toString());
+        collect(x.mid, prefix.append(x.data), matches);
+        prefix.deleteCharAt(prefix.length() - 1);
+        collect(x.right, prefix, matches);
     }
     @Override
     public void addAll(Collection<? extends CharSequence> terms) {
@@ -49,8 +85,7 @@ public class TernarySearchTreeAutocomplete implements Autocomplete {
 
     @Override
     public List<CharSequence> allMatches(CharSequence prefix) {
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        return keysWithPrefix(String.valueOf(prefix));
     }
 
     /**
