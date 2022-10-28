05d90d7e32541e0cbf5768e59563b97d357264fa
Wesley Nguyen <wesngu28@uw.edu>
2022-10-27 18:02:41 -0700

    combine methods to sink and swim and use recursion, implement add

--- src/minpq/OptimizedHeapMinPQ.java
+++ src/minpq/OptimizedHeapMinPQ.java
@@ -31,8 +31,9 @@ public class OptimizedHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         if (contains(item)) {
             throw new IllegalArgumentException("Already contains " + item);
         }
-        // TODO: Replace with your code
-        throw new UnsupportedOperationException("Not implemented yet");
+        items.add(new PriorityNode<>(item, priority));
+        itemToIndex.put(item, items.size()-1);
+        swim(items.size()-1);
     }
 
     @Override
@@ -55,7 +56,7 @@ public class OptimizedHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
         if (isEmpty()) {
             throw new NoSuchElementException("PQ is empty");
         }
-        // TODO: Replace with your code
+        // do something with the sink method
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
@@ -72,5 +73,49 @@ public class OptimizedHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
     public int size() {
         // TODO: Replace with your code
         throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void swim(int index) {
+        if (index == 0) return;
+        int parent = index / 2;
+        double indexPriority = items.get(index).priority();
+        double parentPriority = items.get(parent).priority();
+        while (indexPriority < parentPriority) {
+            swap(index, parent);
+            swim(parent);
+        }
+    }
+
+    public void sink(int index) {
+        if (index == 0) return;
+        double priority = items.get(index).priority();
+        int rightChild =  index * 2 + 1;
+        int leftChild = index * 2;
+        int childIndex = leftChild;
+        double childPriority = items.get(leftChild).priority();
+        if (items.get(rightChild).item() == null && items.get(leftChild).item() == null) {
+            return;
+        }
+        if (items.get(rightChild).item() != null) {
+            if (items.get(rightChild).priority() < items.get(leftChild).priority()) {
+                childPriority = items.get(leftChild).priority();
+                childIndex = leftChild;
+            } else {
+                childPriority = items.get(rightChild).priority();
+                childIndex = rightChild;
+            }
+        }
+        if (childPriority < priority) {
+            swap(index, childIndex);
+            sink(childIndex);
+        }
+    }
+
+    public void swap(int index1, int index2) {
+        PriorityNode temp = items.get(index1);
+        items.set(index1, items.get(index2));
+        items.set(index2, temp);
+        itemToIndex.put(items.get(index2).item(), index2);
+        itemToIndex.put(items.get(index2).item(), index1);
     }
 }
